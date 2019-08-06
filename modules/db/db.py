ad7733d4f9e5bc6bfa2dee9de8ed95be48108e5f
Connor Jardine <2188232j@student.gla.ac.uk>
2019-08-07 00:53:27 +0100

    more ux, chart data errors

--- modules/db/db.py
+++ modules/db/db.py
@@ -4,18 +4,20 @@ competitions_collection = "competitions"
 worlds_collection = "worlds"
 helper_collection = "band"
 
-ss = ", connectTimeoutMS=30000, socketTimeoutMS=None, socketKeepAlive=True,connect=False, maxPoolsize=1"
 
 def pull_data(collection, data=None):
     if collection == "competitions":
         client = pymongo.MongoClient("mongodb+srv://read:readaccess@connor-5cmei.mongodb.net/test?retryWrites=true&w"
-                                     "=majority").rspba.competitions
+                                     "=majority", connectTimeoutMS=30000, socketTimeoutMS=None, socketKeepAlive=True,
+                                     connect=False, maxPoolsize=1).rspba.competitions
     elif collection == "worlds":
         client = pymongo.MongoClient("mongodb+srv://read:readaccess@connor-5cmei.mongodb.net/test?retryWrites=true&w"
-                                     "=majority").rspba.worlds
+                                     "=majority", connectTimeoutMS=30000, socketTimeoutMS=None, socketKeepAlive=True,
+                                     connect=False, maxPoolsize=1).rspba.worlds
     else:
         client = pymongo.MongoClient("mongodb+srv://read:readaccess@connor-5cmei.mongodb.net/test?retryWrites=true&w"
-                                     "=majority").rspba.band_helper_data
+                                     "=majority", connectTimeoutMS=30000, socketTimeoutMS=None, socketKeepAlive=True,
+                                     connect=False, maxPoolsize=1).rspba.band_helper_data
     if not data:
         output = client.find()
     else:
